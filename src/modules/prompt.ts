3a7b71ae499f9d01fc09b90a06f4cee19992abea
peyang-Celeron@bot <42040068+peyang-Celeron@users.noreply.github.com>
2021-01-21 16:35:23 +0000

    Fix codestyle
    Signed-off-by: peyang-Celeron@bot <42040068+peyang-Celeron@users.noreply.github.com>

--- src/modules/prompt.ts
+++ src/modules/prompt.ts
@@ -7,25 +7,20 @@ import manager from "..";
 
 import Module from "./base";
 
-export default class Prompt extends Module 
-{
-    constructor(private parsedArguments: any = {}) 
-    {
+export default class Prompt extends Module {
+    constructor(private parsedArguments: any = {}) {
         super("Prompt", "Show beauty prompts.");
     }
 
-    init(): Promise<void> 
-    {
+    init(): Promise<void> {
         this.enabled = true;
         this.parsedArguments = manager.use("Arguments Manager");
 
         return Promise.resolve();
     }
 
-    use(): (code: number) => void 
-    {
-        return (code: number) => 
-        {
+    use(): (code: number) => void {
+        return (code: number) => {
 
             process.stdout.write(chalk`{bold {blueBright.underline ${this.parsedArguments.host}} as {cyanBright ban-server}${code !== 0 ? chalk.bold(" stopped with " + chalk.redBright(code)) : ""}}\n {magentaBright ${figures.pointer}${code !== 0 ? chalk.redBright(figures.pointer) : chalk.blueBright(figures.pointer)}${figures.pointer}} `);
 
@@ -33,37 +28,30 @@ export default class Prompt extends Module
 
             command = readlineSync.question("").trim();
 
-            while (!command.endsWith(";")) 
-            {
+            while (!command.endsWith(";")) {
                 process.stdout.write(chalk`   {greenBright ${figures.pointer}}     `);
                 command += " " + readlineSync.question("").trim();
             }
 
-            do 
-            {
+            do {
                 command = command.slice(0, -1);
             } while (command.endsWith(";"));
 
             let stopCode;
 
-            try 
-            {
+            try {
                 stopCode = manager.use("Command").commands(command.trim());
-            }
-            catch (error) 
-            {
+            } catch (error) {
                 console.log(chalk`{bgRedBright.black  ERROR } ` + chalk.redBright(__(error.message)));
 
                 stopCode = 1;
             }
 
-            if (stopCode >= 9684) 
-            {
+            if (stopCode >= 9684) {
                 return stopCode - 9684;
             }
 
-            if (stopCode == -1) 
-            {
+            if (stopCode == -1) {
                 console.log(chalk`{bgRedBright.black  ERROR } ` + chalk.redBright(__("Command not found.")));
             }
 
@@ -71,8 +59,7 @@ export default class Prompt extends Module
         };
     }
 
-    close(): Promise<void> 
-    {
+    close(): Promise<void> {
         this.enabled = false;
 
         return Promise.resolve();
