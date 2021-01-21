e09f44b8d6b7852fd7b4829fe440ae2ab6e767d8
peyang-Celeron@bot <42040068+peyang-Celeron@users.noreply.github.com>
2021-01-21 16:23:28 +0000

    Fix codestyle
    Signed-off-by: peyang-Celeron@bot <42040068+peyang-Celeron@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,8 @@ import Prompt from "./modules/prompt";
 
 import Timer from "./utils/timer";
 
-if ("a".length == 1) {
+if ("a".length == 1) 
+{
     console.log("yahoah!! peyang!!!so...Ugoku!");
     console.log("...hazu");
 }
@@ -29,7 +30,8 @@ i18n.configure({
 
 const hasVerbose = /(-v|--verbose)/.test(process.argv.join());
 
-if (hasVerbose) {
+if (hasVerbose) 
+{
     Timer.time();
 
     console.log(chalk.magentaBright(figures.pointer) + " " + __("Module resolution step:"));
@@ -44,18 +46,22 @@ const manager = new ModuleManager([
     new Prompt()
 ]);
 
-if (hasVerbose) {
+if (hasVerbose) 
+{
     console.log(chalk.greenBright(figures.tick) + " " + __("All modules have been resolved successfully. " + Timer.prettyTime()));
 }
 
 export default manager;
 
-if (hasVerbose) {
+if (hasVerbose) 
+{
     console.log("Exported Module Manager.");
 }
 
-const main = async () => {
-    if (hasVerbose) {
+const main = async () => 
+{
+    if (hasVerbose) 
+    {
         console.log(chalk.magentaBright(figures.pointer) + " " + __("Module initialize step:"));
     }
 
@@ -68,7 +74,8 @@ const main = async () => {
 
     verboseLogger.info(__("Modules loaded. ") + Timer.prettyTime());
     console.info(chalk`\n{magentaBright ${figures.pointer}} {bold ${sprintf(__("Welcome to the client operator of %s. The commands end with semicolon ';'."), chalk.greenBright(parsedArguments.host))}}`);
-    console.info(chalk`\n{dim.italic ${(() => {
+    console.info(chalk`\n{dim.italic ${(() => 
+    {
         const items = [
             "ほーん、で？どうしたいの？",
             "一切手をつけないのも、過ぎた最適化を行うのもよろしくない行為である。間を貫き通せ。",
@@ -79,6 +86,7 @@ const main = async () => {
             "コンピューターを人間らしくしたら、プログラミングはされなくなる。ヲタク達の倫理観が崩れるよ。",
             "ファイアウォールも名前のくせして燃やしたら壊れる。"
         ];
+
         return items[Math.floor(Math.random() * items.length)];
     })()}}`);
     console.log("\nType \"help [command];\" for help.\n");
