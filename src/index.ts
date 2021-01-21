20981f4aa40fa901eef9bc120046d9ba4f130784
Peyang <mikasa.chibimasu@gmail.com>
2021-01-21 23:22:21 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import Command from "./modules/command";
 import Prompt from "./modules/prompt";
 
 import Timer from "./utils/timer";
-console.log("hello, pey!");
+console.log("Ah, moo melt cha kougch a dea yoah.");
 prettyError.start();
 i18n.configure({
     locales: [ "en" ],
